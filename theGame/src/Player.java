cc7c4bb4b974add725b22b6f0e46ada45292daea
Jaquiwa <37348681+Jaquiwa@users.noreply.github.com>
2018-03-21 13:15:09 -0600

    Setters/Getters-josh

--- theGame/src/Player.java
+++ theGame/src/Player.java
@@ -1,5 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
 public abstract class Player {
     public int hiScore;
     public int Score;
     public int levelsCompleted;
+    public Player()
+    {
+        hiScore = 0;
+        Score = 0;
+        levelsCompleted = 0;    
+    }
+    
+    public int getHiScore()
+    {
+        return hiScore;
+    }
+    
+    public void setHiScore(int hs)
+    {
+        hiScore = hs;
+    }
+    
+    public int getScore()
+    {
+        return Score;
+    }
+    
+    public void setScore(int s)
+    {
+        Score = s;
+    }
+    
+    public int getLevelsCompleted()
+    {
+        return levelsCompleted;
+    }
+    
+    public void setLevelsCompleted(int lc)
+    {
+        levelsCompleted = lc;
+    }
+
 }
