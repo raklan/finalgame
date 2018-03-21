b5b46bbca45f4db3edb9c6c9d201db6dd18071ca
ColeLutkin <lutkinc@gmail.com>
2018-03-21 13:30:40 -0600

    CardGame

---
+++ theGame/src/CardGamePlayer.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class SpaceInvadersPlayer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CardGamePlayer extends Player
+{
+    public CardGamePlayer(int hs, int s, int lc)
+    {
+        super(hs,s,lc);
+    }
+
+    public void setHighScore(int hs)
+    {
+        hs = hiScore;
+    }
+
+    public void setScore(int s)
+    {
+        s = Score;
+    }
+
+    public void setLevelsCompleted(int l)
+    {
+        l = levelsCompleted;
+    }
+
+    public int getHighScore()
+    {
+        return hiScore;
+    }
+
+    public int getScore()
+    {
+        return Score;
+    }
+
+    public int getLevelsCompleted()
+    {
+        return levelsCompleted;
+    }
+}
